68846c8d6a249052ab7927cb8f467aa056439f01
xiaomo <xiaomo@xiaomo.info>
2020-07-10 16:52:40 +0800

    添加资源文件

---
+++ Source/jump2d/jump2d.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "jump2d.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, jump2d, "jump2d" );
